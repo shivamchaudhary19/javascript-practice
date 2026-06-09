1265f612f7ea5ef24eafeef1894970f2fd167d6d
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-09 23:15:16 +0530

    FreeCodeCamp challenge #303

---
+++ challenges/004-roommate-assignment.js
@@ -0,0 +1,23 @@
+/*Given an array of people with their roommate group, assign hotel rooms.
+
+Rules:
+- People can only share a room with someone from the same group.
+- Pair people in the order they appear within their group.
+- A pair should be returned as "Name1 and Name2".
+- If a person has no roommate, return only their name.
+
+Example:
+
+Input:
+[
+  { name: "Alice", group: "A" },
+  { name: "Bob", group: "B" },
+  { name: "Carol", group: "A" }
+]
+
+Output:
+[
+  "Alice and Carol",
+  "Bob"
+]*/
+

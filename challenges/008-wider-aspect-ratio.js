8d1bdb19f91a30804ffb6d9be470a5b055c2f77f
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-12 22:36:19 +0530

    Daily challenge #292 , freeCodeCamp

---
+++ challenges/008-wider-aspect-ratio.js
@@ -0,0 +1,26 @@
+/*
+Problem: Wider Aspect Ratio
+
+Given two image dimensions in the format "WxH", determine which image has
+the greater width-to-height ratio.
+
+Return the aspect ratio of that image reduced to its simplest form as "W:H".
+
+Example:
+Input: "1920x1080", "800x600"
+Output: "16:9"
+
+Concepts:
+- Strings
+- String Splitting
+- Arrays
+- Math Operations
+- Ratios
+- Greatest Common Divisor (GCD)
+- Functions
+
+Source: FreeCodeCamp
+*/
+
+
+

a959e70382910e74d141774f6517142cff3b7b01
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-14 22:50:18 +0530

    Add challenge 009 - Character Frequency

---
+++ challenges/012-character-frequency.js
@@ -0,0 +1,21 @@
+/*
+Problem: Character Frequency
+
+Given a string, return an object that maps each character to the number
+of times it appears in the string.
+
+Example:
+Input: "test"
+Output: { t: 2, e: 1, s: 1 }
+
+Concepts:
+- Strings
+- Objects
+- Loops
+- Counting Occurrences
+- Key-Value Pairs
+
+Source: FreeCodeCamp
+*/
+
+
